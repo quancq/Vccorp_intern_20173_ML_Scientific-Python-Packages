34026add6e3e2aef5014e516f1e23fdb6d45b3b1
quancq <quancq.it@gmail.com>
2018-07-25 02:08:25 +0700

    Add closet pair algorithm has O(nlogn) complexity and completed ex4

--- ex4.py
+++ ex4.py
@@ -4,16 +4,25 @@
   - Đưa ra hai ngày mà khoảng cách vị trí của nó gần nhau nhất tương ứng với mỗi con ngựa.
 """
 
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
+from ScientificPythonPackages import utils
+import math
+from datetime import datetime
 
+
+def calc_euclidean_distance(x1, y1, x2, y2):
+    return math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
 
 
 if __name__ == '__main__':
     data_path = "./Input/ZebraBotswana.txt"
+    output_dir = "./Ex4_Output"
+    utils.mkdirs(output_dir)
 
     zebra = pd.read_csv(data_path)
     print(zebra.info())
@@ -28,51 +37,89 @@ if __name__ == '__main__':
     animals = pd.concat(animals, axis=1)
 
     print(animals.info())
-
-    # ax = plt.gca()
-
-
-    # scale = 1
+    result_map = {}
 
     for animal in animals.index:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-
-        print("\nAnimal : ", animal)
+        # fig, ax = plt.subplots()
 
         longs = animals.loc[animal]["Long"]
         min_long = np.min(longs)
         max_long = np.max(longs)
-        print("Min Longitude : {}, Max Longitude : {}".format(min_long, max_long))
 
         latts = animals.loc[animal]["Latt"]
         min_latt = np.min(latts)
         max_latt = np.max(latts)
-        print("Min Latitude : {}, Max Latitude : {}".format(min_latt, max_latt))
 
         unix_times = animals.loc[animal]["UnixTime"]
 
         vertices = list(zip(longs, latts))
-        codes = [Path.LINETO for _ in range(len(vertices))]
-        codes[0] = Path.MOVETO
+        num_coordinates = len(vertices)
 
-        print("Number coordinates : ", len(vertices))
+        print("\nAnimal : ", animal)
+        print("Number coordinates : ", num_coordinates)
 
-        path = Path(vertices, codes)
+        # Compute min distance between 2 position by algorithm has O(nlogn) complexity
 
-        patch = patches.PathPatch(path, lw=2)
-        ax.add_patch(patch)
+        # Build points
+        points = []
+        for i in range(num_coordinates):
+            point = utils.Point(vertices[i][0], vertices[i][1], unix_times[i])
+            points.append(point)
 
-        ax.set_xlim(min_long, max_long)
-        ax.set_ylim(min_latt, max_latt)
-        ax.set_xlabel("Longitude")
-        ax.set_ylabel("Latitude")
-        ax.set_title("{}'s moves".format(animal))
+        point1, point2 = utils.find_closet_pair(points)
+        min_distance = utils.calc_euclidean_distance(point1, point2)
+
+        print("Min distance : ", min_distance)
+        print("long1 : {}, latt1 = {}".format(point1.x, point1.y))
+        print("long2 : {}, latt2 = {}".format(point2.x, point2.y))
+        print("Time1 : ", point1.timestamp)
+        print("Time2 : ", point2.timestamp)
+
+        result_map.update({animal: (point1, point2)})
+
+        # codes = [Path.LINETO for _ in range(len(vertices))]
+        # codes[0] = Path.MOVETO
+
+        print("Min Longitude : {}, Max Longitude : {}".format(min_long, max_long))
+        print("Min Latitude : {}, Max Latitude : {}".format(min_latt, max_latt))
+
+        print("Start position : ", vertices[0])
+        print("Finish position : ", vertices[-1])
+
+        # path = Path(vertices, codes)
+
+        # patch = patches.PathPatch(path, lw=2, facecolor=None)
+        # ax.add_patch(patch)
+        #
+        # ax.set_xlim(min_long, max_long)
+        # ax.set_ylim(min_latt, max_latt)
+        # ax.set_xlabel("Longitude")
+        # ax.set_ylabel("Latitude")
+        # ax.set_title("{}'s moves".format(animal))
 
         # Mark start and finish position of animal on map
         # Start position
-        ax.text(vertices[0], vertices[1], )
+        # ax.annotate("Start", xy=vertices[0],
+        #             xytext=(vertices[0][0], vertices[0][1] + 0.1),
+        #             color="r", arrowprops=dict(arrowstyle="->"))
+
+        # Finish position
+        # ax.annotate("Finish", xy=vertices[-1],
+        #             xytext=(vertices[-1][0], vertices[-1][1] + 0.1),
+        #             color="r", arrowprops=dict(arrowstyle="->"))
+
+        # Save figure
+        # save_path = os.path.join(output_dir, "{}.jpg".format(animal))
+        # plt.savefig(save_path, dpi=100)
 
         # break
 
-    plt.show()
+    # Print result
+    print("\n=========================")
+    for animal, (point1, point2) in result_map.items():
+        date1 = datetime.fromtimestamp(point1.timestamp).strftime('%d-%m-%Y %H:%M:%S')
+        date2 = datetime.fromtimestamp(point2.timestamp).strftime('%d-%m-%Y %H:%M:%S')
+        distance = utils.calc_euclidean_distance(point1, point2)
+        print("Animal : {}, date1 : {}, date2 : {}, distance : {}".format(animal, date1, date2, distance))
+
+    # plt.show()
--- utils.py
+++ utils.py
@@ -1,9 +1,208 @@
 import os
 import numpy as np
+import copy
+import math
+import random
+import time
+
 
 def mkdirs(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+
 def rand(low=0, high=1, size=(3,3)):
     return (high - low) * np.random.rand(*size) + low
+
+
+class Point:
+    def __init__(self, x, y, timestamp):
+        self.x = x
+        self.y = y
+        self.timestamp = timestamp
+
+
+def min(a, b):
+    return a if a < b else b
+
+
+def calc_euclidean_distance(point1, point2):
+    return math.sqrt((point2.y - point1.y) ** 2 + (point2.x - point1.x) ** 2)
+
+
+def find_closet_distance_brute_force(points):
+    # Return min distance between all pairs
+    min_distance = calc_euclidean_distance(points[0], points[1])
+    point1 = points[0]
+    point2 = points[1]
+
+    num_points = len(points)
+    for i in range(num_points - 1):
+        for j in range(i + 1, num_points):
+            distance = calc_euclidean_distance(points[i], points[j])
+            if distance < min_distance:
+                min_distance = distance
+                point1 = points[i]
+                point2 = points[j]
+
+    return point1, point2
+
+
+def find_closet_distance_between_two_half(points, max_distance):
+    num_points = len(points)
+    assert num_points >= 2
+
+    min_distance = max_distance
+
+    point1 = points[0]
+    point2 = points[1]
+
+    for i in range(num_points - 1):
+        for j in range(i + 1, num_points):
+            if (points[j].y - points[i].y) >= min_distance:
+                break
+            distance = calc_euclidean_distance(points[i], points[j])
+            if distance < min_distance:
+                min_distance = distance
+                point1 = points[i]
+                point2 = points[j]
+
+    return point1, point2
+
+
+def find_closet_utils(points_sort_by_x, points_sort_by_y):
+    point1 = point2 = Point(0,0,0)
+    num_points = len(points_sort_by_x)
+
+    if num_points <= 3:
+        return find_closet_distance_brute_force(points_sort_by_x)
+
+    # Middle point by x
+    mid_idx = num_points // 2
+    mid_point = points_sort_by_x[mid_idx]
+
+    points_sort_by_y_left = []      # y-coordinate sorted points on left of vertical line
+    points_sort_by_y_right = []     # y-coordinate sorted points on right of vertical line
+
+    for point in points_sort_by_y:
+        if point.x <= mid_point.x:
+            points_sort_by_y_left.append(point)
+        else:
+            points_sort_by_y_right.append(point)
+
+    # Divide and conquer to find closer pair on left and on right
+    point1_left, point2_left = find_closet_utils(
+        points_sort_by_x[:mid_idx],
+        points_sort_by_y_left
+    )
+    min_distance_left = calc_euclidean_distance(point1_left, point2_left)
+
+    point1_right, point2_right = find_closet_utils(
+        points_sort_by_x[mid_idx:],
+        points_sort_by_y_right
+    )
+    min_distance_right = calc_euclidean_distance(point1_right, point2_right)
+
+    if min_distance_left < min_distance_right:
+        point1 = point1_left
+        point2 = point2_left
+        min_distance = min_distance_left
+    else:
+        point1 = point1_right
+        point2 = point2_right
+        min_distance = min_distance_right
+
+
+    candidate_points = []
+    # Find y sorted points which have distance by x in range [mid.x - d, mid.x + d]
+    for point in points_sort_by_y:
+        if math.fabs(point.x - mid_point.x) < min_distance:
+            candidate_points.append(point)
+
+    # Find closet pair in candidate points
+    if len(candidate_points) >= 2:
+        candidate1, candidate2 = find_closet_distance_between_two_half(candidate_points, min_distance)
+
+        min_candidate_distance = calc_euclidean_distance(candidate1, candidate2)
+
+        if min_candidate_distance < min_distance:
+            point1 = candidate1
+            point2 = candidate2
+            min_distance = min_candidate_distance
+
+    return point1, point2
+
+
+def find_closet_pair(points):
+    # Return timestamp pair correspond to closet pair
+
+    print("Find closet pair by algorithm has O(nlogn) complexity ...")
+
+    # Create list of sorted by x-coordinate points
+    points_sort_by_x = copy.deepcopy(points)
+    sorted(points_sort_by_x, key=lambda p: p.x)
+
+    # Create list of sorted by y-coordinate points
+    points_sort_by_y = copy.deepcopy(points)
+    sorted(points_sort_by_y, key=lambda p: p.y)
+
+    point1, point2 = find_closet_utils(
+        points_sort_by_x,
+        points_sort_by_y
+    )
+
+    return point1, point2
+
+
+if __name__ == "__main__":
+    # Test closet pair algorithm
+
+    num_points = 100000
+    points = []
+
+    print("Random points ... ")
+    for i in range(num_points):
+        x = rand(0, 10000, size=(1,))
+        y = rand(0, 10000, size=(1,))
+        timestamp = i
+        point = Point(x, y, timestamp)
+        # print("Point : (x,y,time) =  ({}, {}, {})".format(point.x, point.y, point.timestamp))
+        points.append(point)
+
+    print("Random {} points done".format(num_points))
+
+    start_time = time.time()
+    point1_algo2, point2_algo2 = find_closet_pair(points)
+    finish_time = time.time()
+    min_distance_algo2 = calc_euclidean_distance(point1_algo2, point2_algo2)
+
+    print("Algo has O(nlogn) complexity result: ")
+    print("Point 1 : (x,y,time) =  ({}, {}, {})".format(
+        point1_algo2.x, point1_algo2.y, point1_algo2.timestamp
+    ))
+    print("Point 2 : (x,y,time) =  ({}, {}, {})".format(
+        point2_algo2.x, point2_algo2.y, point2_algo2.timestamp
+    ))
+    print("Min distance : ", min_distance_algo2)
+    print("Time : {} seconds".format((finish_time - start_time)))
+
+    print("==================================================")
+
+    # start_time = time.time()
+    # point1_brute_force, point2_brute_force = find_closet_distance_brute_force(points)
+    # finish_time = time.time()
+    # min_distance_bf = calc_euclidean_distance(point1_brute_force, point2_brute_force)
+    #
+    # print("Brute force result: ")
+    # print("Point 1 : (x,y,time) =  ({}, {}, {})".format(
+    #     point1_brute_force.x, point1_brute_force.y, point1_brute_force.timestamp
+    # ))
+    # print("Point 2 : (x,y,time) =  ({}, {}, {})".format(
+    #     point2_brute_force.x, point2_brute_force.y, point2_brute_force.timestamp
+    # ))
+    # print("Min distance : ", min_distance_bf)
+    # print("Time : {} seconds".format((finish_time - start_time)))
+
+
+
+
